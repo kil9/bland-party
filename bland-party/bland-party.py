d949b9f69d21f63d516d3cc574aca78197559e76
kil9 <chungha.kim@navercorp.com>
2019-04-20 03:21:23 +0900

    Add ratings

--- bland-party/bland-party.py
+++ bland-party/bland-party.py
@@ -1,3 +1,6 @@
+import json
+from collections import OrderedDict
+
 from flask import request, abort
 
 from linebot.exceptions import (
@@ -7,7 +10,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-from config import *
+from config import app, r, line_bot_api, handler
+
+ENTRY_RATINGS = 'ratings'
 
 
 @app.route("/callback", methods=['POST'])
@@ -31,10 +36,56 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if '!강등' in event.message.text:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='강등입니다'))
+    if len(event.message.text) == 0:
+        app.logger.warn('too short message to split: ' + event.message.text)
+        return
+
+    splitted = event.message.text.split()
+    if splitted[0] == '!리셋':
+        r.delete(ENTRY_RATINGS)
+
+    if splitted[0] == '!강등':
+        demote(event)
+
+
+def ratings_to_message(ratings):
+    str = ''
+    for person, rating in ratings.items():
+        str += '{} {}\n'.format(rating, person)
+    return str
+
+
+def demote(event):
+
+    splitted = event.message.text.split()
+    try:
+        ratings_txt = r.get(ENTRY_RATINGS)
+        ratings = json.loads(ratings_txt, object_pairs_hook=OrderedDict)
+    except Exception as e:
+        app.logger.exception(e)
+        ratings = OrderedDict()
+
+    if len(splitted) < 3:
+        app.logger.warn('too short message to demote')
+        return
+
+    if len(splitted) >= 3:
+        to_demote = splitted[1]
+        demote_rating = splitted[2]
+
+    if to_demote in ratings:
+        del ratings[to_demote]
+
+    ratings[to_demote] = demote_rating
+
+    message = ratings_to_message(ratings)
+
+    ratings_entry = json.dumps(ratings)
+    r.set(ENTRY_RATINGS, ratings_entry)
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=message))
 
 
 if __name__ == "__main__":
--- bland-party/config.py
+++ bland-party/config.py
@@ -1,8 +1,10 @@
 import os
 
-from linebot import LineBotApi, WebhookHandler
+import redis
 
 from flask import Flask
+
+from linebot import LineBotApi, WebhookHandler
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,8 +12,10 @@ load_dotenv()
 LINE_CHANNEL_ID = os.getenv("LINE_CHANNEL_ID")
 LINE_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+REDIS_URL = os.environ.get("REDIS_URL")
 
 line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+r = redis.from_url(REDIS_URL)
 
 app = Flask(__name__)
