4a3341a022c12c9207b45637e1ae031a21e600b7
kil9 <chungha.kim@navercorp.com>
2019-05-31 21:47:27 +0900

    Add images

--- bland-party/images.py
+++ bland-party/images.py
@@ -10,6 +10,7 @@ def get_special_images(special: str) -> str:
         'critical': (
             'https://img.insight.co.kr/static/2017/05/22/700/21dj9r72yx01d4p9ii9x.jpg',
             'https://t1.daumcdn.net/cfile/tistory/223F70475298764C32',
+            'https://cdn.pixabay.com/photo/2017/12/20/07/04/portrait-of-a-rock-kestrel-3029352_960_720.jpg',  # noqa: E501
             ),
         }
 
